f0c30843ebae0cac2b434dde0aaec75cee2cd63a
hezhanghe <hezhanghe@hzjytech.com>
2017-10-23 19:05:20 +0800

    start iteratorpattern

---
+++ app/src/main/java/com/hzjytech/hades/desginpattern/iteratorpattern/AbstractObjectList.java
@@ -0,0 +1,111 @@
+package com.hzjytech.hades.desginpattern.iteratorpattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Hades on 2017/10/23.
+ */
+
+abstract class AbstractObjectList {
+
+    protected List<Object> objects=new ArrayList<>();
+
+    public AbstractObjectList(List objects){
+        this.objects=objects;
+    }
+
+    public void addObject(Object obj){
+        objects.add(obj);
+    }
+
+    public void removeObject(Object obj){
+        this.objects.remove(obj);
+    }
+
+    public abstract AbstractIterator createIterator();
+
+    public List getObjects(){
+        return this.objects;
+    }
+
+}
+
+class ProductList extends AbstractObjectList{
+
+    public ProductList(List objects) {
+        super(objects);
+    }
+
+    @Override
+    public AbstractIterator createIterator() {
+        return new ProductIterator(this);
+    }
+}
+
+class ProductIterator implements AbstractIterator{
+
+    private final ProductList productList;
+    private final List products;
+    private int cursor1;
+    private int cursor2;
+
+    public ProductIterator(ProductList productList) {
+        this.productList=productList;
+        this.products=productList.getObjects();
+        cursor1=0;
+        cursor2=products.size()-1;
+    }
+
+    @Override
+    public void next() {
+        if(cursor1<products.size()){
+            cursor1++;
+        }
+
+    }
+
+    @Override
+    public boolean isLast() {
+        return (cursor1==products.size());
+    }
+
+    @Override
+    public void previous() {
+        if(cursor2>-1){
+            cursor2--;
+        }
+
+    }
+
+    @Override
+    public boolean isFirst() {
+        return (cursor2==-1);
+    }
+
+    @Override
+    public Object getNextItem() {
+        return products.get(cursor1);
+    }
+
+    @Override
+    public Object getPreviousItem() {
+        return products.get(cursor2);
+    }
+}
+
+interface AbstractIterator{
+    public void next();
+
+    public boolean isLast();
+
+    public void previous();
+
+    public boolean isFirst();
+
+    public Object getNextItem();
+
+    public Object getPreviousItem();
+
+
+}
--- app/src/test/java/com/hzjytech/hades/desginpattern/singlepattern/example/PrinterDriverTest.kt
+++ app/src/test/java/com/hzjytech/hades/desginpattern/singlepattern/example/PrinterDriverTest.kt
@@ -1,7 +1,14 @@
 package com.hzjytech.hades.desginpattern.singlepattern.example
 
+import android.os.Environment
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
 import org.junit.Assert.*
 import org.junit.Test
+import java.io.File
+import java.io.RandomAccessFile
+import java.nio.charset.Charset
 
 /**
  * Created by Hades on 2017/9/25.
@@ -29,6 +36,76 @@ class Son: Only() {
 
     fun showI():Unit{
         println("your son is "+i+" years old")
+    }
+
+}
+
+
+class LogFilePlugin {
+    companion object {
+        var OPEN = false
+
+        private val ABSOLUTE_CRASH_LOG_PATH = "${Environment.getExternalStorageDirectory().absolutePath}/log/alltest.log"
+        private val lock: Any = Any()
+        private val fileThreadHandler: Handler by lazy { Handler(HandlerThread("log_file_thread").apply { start() }.looper) }
+
+        init {
+            if (!File(ABSOLUTE_CRASH_LOG_PATH).parentFile.exists()) File(ABSOLUTE_CRASH_LOG_PATH).parentFile.mkdirs()
+        }
+
+        private fun appendLog(content: String) {
+            synchronized(lock) {
+                val file = RandomAccessFile(ABSOLUTE_CRASH_LOG_PATH, "rw")
+                try {
+                    Log.e("LOG R APPEND", content)
+                    file.seek(file.length()?:0)
+                    file.write("$content\n".toByteArray(Charset.defaultCharset()))
+                    file.close()
+                    Log.e("LOG R APPEND-ok", content)
+                } catch (ex: Exception) {
+                    ex.printStackTrace()
+                }
+            }
+        }
+
+        fun AppendLog(content: String) {
+            if (!OPEN) return
+            Log.e("LOG R", content)
+            fileThreadHandler.post { appendLog("${content}") }
+        }
+
+        fun AppendLog(key: String, content: String) {
+            if (!OPEN) return
+            fileThreadHandler.post { appendLog("$key:$content") }
+        }
+
+        fun RLog(tag: String, msg: String) {
+            if (!OPEN) return
+            fileThreadHandler.post { appendLog("Log/R$tag:$msg") }
+        }
+
+        fun asyncAllText(succ: (content: String) -> Unit) {
+            fileThreadHandler.post {
+                synchronized(lock) {
+                    val file = RandomAccessFile(ABSOLUTE_CRASH_LOG_PATH, "rw")
+                    val bs: ByteArray = kotlin.ByteArray(file.length().toInt())
+                    if (file.length() > 0) {
+                        file.readFully(bs)
+//                        Framework._MAIN_HANDLER.post { succ(String(bs, Charset.defaultCharset())) }
+                    }
+                }
+            }
+        }
+
+        fun clear() {
+            fileThreadHandler.post {
+                val file = File(ABSOLUTE_CRASH_LOG_PATH)
+                synchronized(lock) {
+                    if (file.exists()) file.delete()
+                }
+            }
+        }
+
     }
 
 }
