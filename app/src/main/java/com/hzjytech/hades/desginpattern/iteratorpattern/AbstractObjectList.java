9e7b0236897350cb1fdc86484d1705a15f94a117
zhanghehe <hezhanghe@126.com>
2017-10-28 15:44:52 +0800

    start CommandPattern

--- app/src/main/java/com/hzjytech/hades/desginpattern/iteratorpattern/AbstractObjectList.java
+++ app/src/main/java/com/hzjytech/hades/desginpattern/iteratorpattern/AbstractObjectList.java
@@ -107,5 +107,60 @@ interface AbstractIterator{
 
     public Object getPreviousItem();
 
+}
 
+class CustomList extends AbstractObjectList{
+
+    public CustomList(List users){
+        super(users);
+    }
+
+    @Override
+    public AbstractIterator createIterator() {
+        return new UserIterator();
+    }
+
+    private class UserIterator implements AbstractIterator{
+        private int cursor1;
+        private int cursor2;
+
+        public UserIterator(){
+            cursor1=0;
+            cursor2=objects.size()-1;
+        }
+
+        @Override
+        public void next() {
+            if(cursor1<objects.size()){
+                cursor1++;
+            }
+        }
+
+        @Override
+        public boolean isLast() {
+            return (cursor1==objects.size());
+        }
+
+        @Override
+        public void previous() {
+            if(cursor2>-1){
+                cursor2--;
+            }
+        }
+
+        @Override
+        public boolean isFirst() {
+            return (cursor2==-1);
+        }
+
+        @Override
+        public Object getNextItem() {
+            return objects.get(cursor1);
+        }
+
+        @Override
+        public Object getPreviousItem() {
+            return objects.get(cursor2);
+        }
+    }
 }
